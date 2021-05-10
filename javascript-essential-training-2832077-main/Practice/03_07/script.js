f108374aadb92eb4d3b6ff66b7e8375886f7fc08
Lauren Black <laurenblack042699@gmail.com>
2021-05-10 00:24:10 -0400

    practicing objects, properties and methods

--- javascript-essential-training-2832077-main/Practice/03_07/script.js
+++ javascript-essential-training-2832077-main/Practice/03_07/script.js
@@ -7,3 +7,34 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const wallet = {
+    name: "Lauren's Wallet",
+    cash: 50,
+    cards: ["debit card","credit card","scene card","starbucks card","optimum card"],
+    color: "brown",
+
+    starbucksCard1: {
+        style: "generic",
+        color: "white",
+        amount: 20,
+        used: false,
+
+        toggleUsed: function(usedStatus) {
+            this.used = usedStatus
+        },
+    },
+
+    starbucksCard2: {
+        style: "christmas",
+        color: "multi",
+        amount: 30,
+        used: false,
+        toggleUsed: function(usedStatus) {
+            this.used = usedStatus
+        },
+    },
+    updateCash: function(cashStatus) {
+        this.cash = cashStatus
+    },
+}
