8b3599451a6c229de33b5e3c415451ee8205fdde
Lauren Black <laurenblack042699@gmail.com>
2021-06-05 19:14:35 -0400

    functions

--- javascript-essential-training-2832077-main/Practice/08_06/script.js
+++ javascript-essential-training-2832077-main/Practice/08_06/script.js
@@ -5,3 +5,25 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+const hipPack = {
+    name: "Hip Pack",
+    pocketNum: 2,
+    color: "Pink",
+}
+
+const addPack = (newPack) => {
+    const newArticle = document.createElement("article");
+    newArticle.innerHTML = `
+    <h1>${newPack.name}</h1>
+    <ul>
+        <li><a>Number of Pockets: ${newPack.pocketNum}</a></li>
+        <li><a>Colour: ${newPack.color}</a></li>
+    </ul>
+    `
+
+    return newArticle;
+}
+
+const main = document.querySelector("main");
+main.append(addPack(hipPack));
