bc60e98528b84d9202b938fbd97e28ccf6da13b6
Lauren Black <laurenblack042699@gmail.com>
2021-06-05 21:26:38 -0400

    Adding context description

--- javascript-essential-training-2832077-main/08_15/script.js
+++ javascript-essential-training-2832077-main/08_15/script.js
@@ -9,6 +9,8 @@ const article = document.querySelector("article");
 let stuffList = document.createElement("ul");
 
 // forEach() array method
+//Map would not typically be used like this. Map is used when you need to loop through a manipulate data to create a new array.
+//If you just want to loop through and display data, use forEach.
 const stuffItmes = stuff.map((item) => {
   let listItem = document.createElement("li");
   listItem.innerHTML = item;
